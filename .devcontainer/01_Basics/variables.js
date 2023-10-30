49bbc5db024cf48c5c35296e329c4115aad4c995
kirandhongadi7 <148469752+kirandhongadi7@users.noreply.github.com>
2023-10-30 16:21:19 +0000

    container config done

---
+++ .devcontainer/01_Basics/variables.js
@@ -0,0 +1,8 @@
+const accountId = "112233"
+console.log(accountId);
+
+let accountEmail = "kiran@gmail.com"
+let accountpassward = "7799"
+accountCity = "bagalkot"
+
+console.table([accountEmail, accountId, accountpassward, accountCity])
